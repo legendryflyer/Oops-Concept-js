03ffe01416d3c5ec4c9be5d35a9b7e7c871b41eb
legendryflyer <tavishanade12@gmail.com>
2024-02-07 12:48:54 +0530

    added

--- day 4.js
+++ day 4.js
@@ -31,19 +31,35 @@ console.log(obj.save())
 
 // overloding  ---> same method ---- > same classes --> same parameter -- > different results
 
+// class calculator{
+//     add(a,b){
+//         return a+b
+//     }
+//     add(a,b,c){
+//         return a+b+c
+//     }
+//     add(a,b,c,d){
+//         return a+b+c+d
+//     }
+// }
+
+// let s= new calculator()
+// console.log(s.add(1,2))
+// console.log(s.add(3,4))
+// console.log(s.add(5,6,7))
+
+
+
 class calculator{
-    add(a,b){
-        return a+b
-    }
-    add(a,b,c){
-        return a+b+c
-    }
     add(a,b,c,d){
-        return a+b+c+d
+        if(a!=undefined  && b!= undefined && c!=undefined && d!=undefined){
+            return a+b+c+d
+        }
+        else if(a!=undefined && b!=undefined && c!=undefined){
+            return  a+b+c
+        }
+        else if(a != undefined && b != undefined){
+            return  a + b
+        }    
     }
 }
-
-let s= new calculator()
-console.log(s.add(1,2))
-console.log(s.add(3,4))
-
