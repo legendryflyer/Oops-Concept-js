aa60b10e7edee8493fc16c256ea71be116e63e46
legendryflyer <tavishanade12@gmail.com>
2024-02-13 09:36:47 +0530

    added

--- lexical scope2.js
+++ lexical scope2.js
@@ -40,7 +40,74 @@ function multi(){
         console.log(x*y)
     }
 }
-let q2=function(){
-    console.log(x*y)
-}
+// let q2=function(){
+//     console.log(x*y)
+// }
 q2()
+
+// actual differencre between the arrow function and function expression
+
+
+// function addition(x,y){
+    //     console.log(x+y)
+    // }
+    // addition(12,4)
+    
+    // let additionB = function(x,y){
+    //     console.log(x+y)
+    // }
+    // additionB(13,4)
+    
+    // let additionC = (x,y) => {
+    //     console.log(x+y)
+    // }
+    // additionC(13,4)
+    
+    var firstName = "tanmay"
+    var lastName = "kulkarni"
+    
+    // let info = {
+    //     firstName:"chinmay",
+    //     lastName:"deshpande",
+    //     display:function(){
+    //         //console.log(this)
+    //         console.log(this.firstName + this.lastName)
+    //         let display2 = function(){
+    //             console.log(this)
+    //             console.log(this.firstName + this.lastName)
+    //         }
+    //         display2()
+    //     }
+    // }
+    // info.display()
+
+
+// let info = {
+//         firstName:"chinmay",
+//         lastName:"deshpande",
+//         display:function(){
+//             //console.log(this)  -- info
+//             console.log(this.firstName + this.lastName)
+//             let display2 = () => {
+//                 //console.log(this) -- info
+//                 console.log(this.firstName + this.lastName)
+//             }
+//             display2()
+//         }
+//     }
+// info.display()
+
+// let info = {
+//     firstName: "chinmay",
+//     lastName: "deshpande",
+//     display: () => {
+//         //console.log(this)  --- window
+//         console.log(this.firstName + this.lastName)
+//         let display2 = () => {
+//             //console.log(this)  -- window
+//             console.log(this.firstName + this.lastName)
+//         }
+//         display2()
+//     }
+// }
+// info.display()
